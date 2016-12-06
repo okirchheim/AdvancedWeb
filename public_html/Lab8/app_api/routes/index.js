0579d130d015880c34d8b6f64b4b81eaef79e068
okirchheim <ookirchheim@email.neit.edu>
2016-12-05 22:41:28 -0500

    Add files via upload

---
+++ public_html/Lab8/app_api/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var ctrlEmployees = require('../controllers/employees');
+
+// employees
+router.get('/employees', ctrlEmployees.employeesReadAll);
+router.get('/employees/:employeesid', ctrlEmployees.employeesReadOne);
+router.post('/employees', ctrlEmployees.employeesCreate);
+router.put('/employees/:employeesid', ctrlEmployees.employeesUpdateOne);
+router.delete('/employees/:employeesid', ctrlEmployees.employeesDeleteOne);
+
+module.exports = router;
